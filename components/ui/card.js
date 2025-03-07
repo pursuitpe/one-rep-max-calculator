9ba6d4736825e132a57345f93225164957f63fab
pursuitpe <pursuitpe@gmail.com>
2025-03-07 07:48:10 -0600

    Create card.js

---
+++ components/ui/card.js
@@ -0,0 +1,7 @@
+export function Card({ children, className }) {
+  return <div className={`bg-white p-4 rounded-lg shadow ${className}`}>{children}</div>;
+}
+
+export function CardContent({ children, className }) {
+  return <div className={`p-2 ${className}`}>{children}</div>;
+}
